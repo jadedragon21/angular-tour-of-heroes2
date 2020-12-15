b9552969a76f81fb7b002fe8f1340cfe92b3805a
jadedragon21 <74530173+jadedragon21@users.noreply.github.com>
2020-12-15 16:38:15 -0600

    Finished Step 5 start on step 6

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange' },
+  { id: 12, name: 'Black Panther' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Spider Man' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'Black Widow' },
+  { id: 18, name: 'Wolverine' },
+  { id: 19, name: 'Deadpool' },
+  { id: 20, name: 'Jean Gray' }
+];
